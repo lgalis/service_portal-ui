deacb27cbbdce3595bc9663df1b531d0551b29b4
Laura Galis <lgalis@redhat.com>
2020-04-30 15:00:48 -0400

    Update parameter for the error notification to requestID

--- src/helpers/shared/user-login.js
+++ src/helpers/shared/user-login.js
@@ -28,7 +28,7 @@ const resolveInterceptor = (response) => response.data || response;
 const errorInterceptor = (error = {}) => {
   const requestId = error.response?.headers['x-rh-insights-request-id'];
   throw requestId
-    ? { ...error.response, sentryId: requestId }
+    ? { ...error.response, requestId: requestId }
     : { ...error.response };
 };
 
